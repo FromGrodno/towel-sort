4051e12824b2d8048b15eb7167122b7f21860e4c
Hancharuk Aliaksei <hancharuk19257@gmail.com>
2022-02-21 17:53:43 +0300

    sfdvs

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-  if (matrix == null) {
+  if (matrix == undefined) {
     return []
   }
   else {
     let result = []
-    for (let i = 0; i < matrix.length; i++) {
+    for (let i = 0; i < matrix.length; i = i + 1) {
       if (i % 2 !== 0) {
         matrix[i].reverse()
       }
-      for (let j = 0; j < matrix[i].length; j++) {
+      for (let j = 0; j < matrix[i].length; j = j + 1) {
         result.push(matrix[i][j]);
 
       }
